30796e0b6e9622bbf6e1df0f1d47fda502fe583b
Nur <nur.hilal@trailblu.com>
2022-09-02 10:58:57 +0300

    whatever

--- dummy2/indexCopy.js
+++ dummy2/indexCopy.js
@@ -1,17 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+// <!DOCTYPE html>
+{/* <html lang="en"> */}
 
-<head>
+{/* <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>My Forms</title>
-</head>
+</head> */}
 
 <body>
 	<center>
 		<h1> Version 1 - Original</h1>
-		<!-- <img src="https://storage.googleapis.com/twg-content/images/MarketingResources_Thumbnail_Search.width-1200.jpg"> -->
+		{/* <!-- <img src="https://storage.googleapis.com/twg-content/images/MarketingResources_Thumbnail_Search.width-1200.jpg"> --> */}
 		<div style="padding: 20px;">
 			<h3> double click this button</h3>
 			<div><button id="notunique" class="duplicated" name="dblclkme"
@@ -21,7 +21,7 @@
 		<h2>Original State</h2>
 		<table>
 			<tr>
-				<!-- <td><button onclick="style='background-color:green'">Just a button</button></td> -->
+				{/* <!-- <td><button onclick="style='background-color:green'">Just a button</button></td> --> */}
 				<td><button id="btn1" class="Col1" name='button1' onclick="style='background-color:green'">
 						All unique Attributes</button></td>
 				<td><button id="btn2" class="Col2" onclick="style='background-color:green'">
@@ -62,12 +62,12 @@
 
 		<div>
 			<form action="https://www.youtube.com/results">
-				<!-- https://www.youtube.com/results -->
+				{/* <!-- https://www.youtube.com/results --> */}
 				<span>
 
 					<p>
 						<label for="search">what are you looking for in youtube</label>
-						<input type="text" id="01" placeholder="search youtube" name="search_query">
+						<input type="text" id="01" placeholder="search youtube" name="search_query"></input>
 
 					</p>
 					<p>
@@ -83,7 +83,7 @@
 				<span>
 					<p>
 						<label for="search">what are you looking for in Google </label>
-						<input type="text" id="02" placeholder="search_google" name="q">
+						<input type="text" id="02" placeholder="search_google" name="q"></input>
 
 					</p>
 					<p>
@@ -117,18 +117,25 @@
 
 		</div>
 
-		<!-- <h2>Alert Testing</h2>
+		{/* <!-- <h2>Alert Testing</h2>
         <div id='alertTesting'>
             <button type="button" id='alertBtn' onclick="alert('Warning this is just alert')"> simple alert</button>
             <button type="button" id='confirmBtn' onclick="confirm('To confirm or not to confirm')">confirm
                 alert</button>
             <button type="button" data-id='promptBtn' onclick="prompt('enter anything')">prompt alert</button>
-        </div> -->
+        </div> --> */}
 
-
+		<div style="margin-top: 20px;">
+			<select name="cars" id="cars" value={(e) => { e.target.value }}>
+				<option value={0}>Volvo</option>
+				<option value={1}>Saab</option>
+				<option value={2}>Mercedes</option>
+				<option value={3}>Audi</option>
+			</select>
+		</div>
 
 	</center>
 
 </body>
 
-</html>
+// </html>
